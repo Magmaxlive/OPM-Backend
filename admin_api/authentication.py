3865aceae7deddb1d62e001b5e5642e59f4fcfe8
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-07-01 15:51:33 +0530

    changed auth view

--- admin_api/authentication.py
+++ admin_api/authentication.py
@@ -1,10 +1,17 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         token = request.COOKIES.get('access_token')
         if not token:
             return None
-        validated = self.get_validated_token(token)
-        user = self.get_user(validated)
-        return (user,validated)
+        
+        try:
+            validated = self.get_validated_token(token)
+            user = self.get_user(validated)
+            return (user, validated)
+        
+        except (InvalidToken, TokenError):
+            return None
+       
